c20b45cb4798d1aae326ed5bcc1992d881f5b7b5
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-08-09 15:50:52 -0500

    done except text only input

--- strings.js
+++ strings.js
@@ -2,8 +2,21 @@ console.log("hi");
 
 let input = document.getElementById("inputArea");
 let output = document.getElementById("outputArea");
+let output2 = document.getElementById("outputArea2");
+let output3 = document.getElementById("outputArea3");
+// let input2 = input.value;
 let button = document.getElementById("button");
 var testString = "";
+let sentence = "";
+
+var charsToRemove = [/\0/g, /\1/g, /\2/g, /\3/g, /\4/g, /\5/g, /\6/g, /\7/g, /\8/g, /\9/g];
+
+
+// for (var i = 0; i < charsToRemove.length; i++) {
+// 	var removeCharacters = charsToRemove[i];
+// 	input = input.replace(removeCharacters, "");
+// }
+
 
 
 function inputAsString() {
@@ -13,37 +26,72 @@ function inputAsString() {
 
 
 function reversal() {
-  	testString = testString.split("").reverse("").join("");
-  	return testString;
+  	testString1 = testString.split("").reverse("").join("");
+  	return testString1;
+}
+
+function alphabits() {
+	testString2 = testString.split("").sort().join("");
+	return testString2;
+}
+
+function palindrome() {
+	if (input.value === testString1) {
+		sentence = "Your word is a palindrome!";
+	} else {
+		sentence = "I'm sorry, your word is not a palindrome.";
+	}
+	return sentence;
 }
 
 button.addEventListener("click", function() {
+
+for (var i = 0; i < charsToRemove.length; i++) {
+	// var removeCharacters = charsToRemove[i];
+	input.value = input.value.replace(charsToRemove[i], "");
+}
 	
-inputAsString();
-reversal();
+	inputAsString();
+	reversal();
+	alphabits();
+	palindrome();
 
-output.innerHTML = testString;
-
-})
-
-
-
-
-
+	output.innerHTML = testString1;
+	output2.innerHTML = testString2;
+	output3.innerHTML = sentence;
+	});
 
 
+input.addEventListener("keyup", function(event) {
 
-function alphabits() {
+	if (event.keyCode === 13) {
 
+	for (var i = 0; i < charsToRemove.length; i++) {
+	// let removeCharacters = charsToRemove[i];
+	input.value = input.value.replace(charsToRemove[i], "");
+	}
+
+	inputAsString();
+	reversal();
+	alphabits();
+	palindrome();
+
+	output.innerHTML = testString1;
+	output2.innerHTML = testString2;
+	output3.innerHTML = sentence;
 }
 
+});
 
 
 
 
-function palindrome() {
 
-}
+
+
+
+
+
 
 
 
